e94f907d25d5f70bee7d2b76d75656e5277621ba
Ian-Caliel <ian.caliel@academico.ifpb.edu.br>
2024-03-06 17:43:20 -0300

    aula 06/03/2024

---
+++ APE/desafio.py
@@ -0,0 +1,5 @@
+valor = float(input('valor da compra: '))
+cupons = int(valor / 30)
+print (f'Quantidade de cupom: {cupons}')
+print (f'falta {cupons} cupon(s)')
+falta = 30-valor
--- APE/questão 8.py
+++ APE/questão 8.py
@@ -1,3 +1,4 @@
+nome = input('digite seu nome: ')
 nota1 = input('Digite nota 1: ')
 nota1 = int(nota1)
 
@@ -9,4 +10,5 @@ nota3 = int(nota3)
 
 media = (nota1 + nota2 + nota3) / 3
 
-print('media:', media)
+print('media de',nome, '=', f"{media:.2f}") 
+
