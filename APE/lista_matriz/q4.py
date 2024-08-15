1731614e338145a6f4e4369c33288e447f42963e
Ian-Caliel <ian.caliel@academico.ifpb.edu.br>
2024-08-15 18:09:13 -0300

    Update q4.py

--- APE/lista_matriz/q4.py
+++ APE/lista_matriz/q4.py
@@ -1,8 +1,6 @@
 import random
 
 matriz = [0] * 20
-
-# Preencher matriz
 soma_medias = 0
 for i in range(20):
     notas = [random.randint(0,100) for _ in range(3)] + [0]
@@ -14,12 +12,10 @@ for i in range(20):
 media_geral = round((soma_medias / 20), 1)
 acima_media = 0
 
-# Checar alunas acima da media
 for i in range(20):
     if matriz[i][-1] > media_geral:
         acima_media += 1
 
-# Exibir matriz
 print("[Nota1,Nota2,Nota3,Media]")
 print(*matriz, sep='\n')
 print("Media geral:", media_geral)
